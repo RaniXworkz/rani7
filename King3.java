4aa9fdc4f223bb5f71a10a6d4ddf6389f2db2f24
RaniXorkz <raniattigeri.xworkz@gmail.com>
2021-12-20 23:41:49 +0530

    hii abhijna

---
+++ King3.java
@@ -0,0 +1,14 @@
+class King
+{
+public static void main(String[] b)
+{
+long []ids={1324245,14257878,15678,34567,123456789l};
+System.out.println(ids[0]);
+System.out.println(ids[1]);
+System.out.println(ids[2]);
+System.out.println(ids[3]);
+System.out.println(ids[4]);
+}
+}
+
+
